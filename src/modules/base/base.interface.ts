c248ab03fed9e730a333b21565be79d3d2e7277a
itsnicolearias <nicolmccann@gmail.com>
2025-07-30 12:33:47 -0300

    feat: add base module

---
+++ src/modules/base/base.interface.ts
@@ -0,0 +1,43 @@
+import { Request, Response, NextFunction } from 'express';
+
+export interface IBaseService {
+  getAll(
+    professionalId?: string,
+    includeModel?: object,
+    page?: number,
+    size?: number,
+    where?: object,
+  ): Promise<any>;
+
+  getOne(
+    where: object,
+    includeModel?: any,
+    professionalId?: string,
+  ): Promise<any>;
+
+  create(
+    body: object,
+    professionalId?: string,
+  ): Promise<any>;
+
+  update(
+    body: object,
+    where: object,
+    professionalId?: string,
+  ): Promise<any>;
+
+  delete(
+    where: object,
+    professionalId?: string,
+  ): Promise<any>;
+}
+
+
+
+export interface IBaseController {
+  getAll(req: Request, res: Response, next: NextFunction): Promise<void>;
+  getOne(req: Request, res: Response, next: NextFunction): Promise<void>;
+  create(req: Request, res: Response, next: NextFunction): Promise<void>;
+  update(req: Request, res: Response, next: NextFunction): Promise<void>;
+  delete(req: Request, res: Response, next: NextFunction): Promise<void>;
+}
---
+++ src/modules/base/base.service.ts
@@ -0,0 +1,179 @@
+import boom from '@hapi/boom';
+import moment from 'moment';
+import { IBaseService } from '../base/base.interface';
+
+
+class BaseService implements IBaseService {
+  model: any;
+
+  constructor(model: any) {
+    this.model = model;
+  }
+
+  /**
+   * @description method to get all the records easily, this is done so that just by extending the base class, you have access to an entire crud
+   * @param professionalId
+   * @param includeModel
+   * @param page
+   * @param size
+   * @param where
+   * @param all
+   * @returns {HttpResponse.getSuccessful | boom.badRequest}
+   */
+  public async getAll(
+    professionalId?: string,
+    includeModel?: object,
+    page?: number,
+    size?: number,
+    where?: Record<string, unknown>,
+    all?: boolean,
+  ) {
+    try {
+      let whereCondition = { ...where };
+      if (professionalId) {
+        whereCondition.professionalId = professionalId;
+      }
+
+      const limit = size || 10;
+      const offset = page ? (page - 1) * limit : 0;
+
+      const records = await this.model.findAndCountAll({
+          where: whereCondition,
+          limit,
+          offset,
+          include: includeModel,
+          subQuery: false,
+          distinct: true,
+        });
+
+        const pages = Math.ceil(records.count / limit);
+        records.pagesQuantity = pages;
+
+        return records;
+    } catch (e) {
+      throw boom.badRequest(e);
+    }
+  }
+
+  /**
+   * @description method to easily obtain a record through a column, this is done so that by simply extending the base class, you have access to an entire crud
+   * @param professionalId
+   * @param where
+   * @param includeModel
+   * @returns
+   */
+  async getOne(
+    where: Record<string, unknown>,
+    includeModel?: object,
+    professionalId?: string,
+  ) {
+    try {
+      if (professionalId) {
+        where.professionalId = professionalId;
+      }
+
+      const record = await this.model.findOne({
+        where,
+        include: includeModel,
+      });
+
+      if (!record) throw boom.notFound(`${this.model.name} not found`);
+
+      return record;
+    } catch (e) {
+      throw boom.badRequest(e);
+    }
+  }
+
+  /**
+   * basic method for create a record in the db, body must arrive validated
+   * @param professionalId
+   * @param body
+   * @param include
+   * @returns {HttpResponse.postSuccessful | boom.badRequest}
+   */
+  async create(body: any, professionalId?: string, include?: any[]) {
+    try {
+      if (professionalId) {
+        body.professionalId = professionalId;
+      }
+
+      const record = await this.model.create({
+        data: body,
+        include,
+      });
+
+      return record;
+    } catch (e) {
+      throw boom.badRequest(e);
+    }
+  }
+
+  /**
+   * update a record in Bd
+   * @param professionalId
+   * @param body
+   * @param where
+   * @returns {HttpResponse.postSuccessful | boom.badRequest}
+   */
+  async update(body: any, where: Record<string, unknown>, professionalId?: string) {
+    try {
+      const record = await this.model.findOne({ where });
+
+      if (!record) throw boom.notFound(`${this.model.name} not found`);
+
+      if (professionalId && record.professionalId !== professionalId) {
+        throw boom.unauthorized('You are not authorized to update this record');
+      }
+
+      body.updatedAt = moment().toISOString();
+
+      const updatedRecord = await this.model.update({
+        where,
+        data: body,
+      });
+
+      return updatedRecord;
+    } catch (e) {
+      throw boom.badRequest(e);
+    }
+  }
+
+  /**
+   * basic method for delete a record in the db
+   * @param where
+   * @param professionalId
+   * @param physicalDestroy
+   * @returns {HttpResponse.deleteSuccessful | boom.badRequest}
+   */
+  async delete(
+    where: Record<string, unknown>,
+    professionalId?: string,
+    physicalDestroy?: boolean,
+  ) {
+    try {
+      const record = await this.model.findOne({ where });
+
+      if (!record) throw boom.notFound(`${this.model.name} not found`);
+
+      if (professionalId && record.professionalId !== professionalId) {
+        throw boom.unauthorized('You are not authorized to delete this record');
+      }
+
+      if (physicalDestroy) {
+        await this.model.delete({ where });
+        return { message: 'Record deleted successfully' };
+      }
+
+      const updatedRecord = await this.model.update({
+        where,
+        data: { deleted: true, updatedAt: moment().toISOString() },
+      });
+      return { message: 'Record deleted successfully', record: updatedRecord };
+    } catch (e) {
+      throw boom.badRequest(e);
+    }
+  }
+}
+
+export default BaseService;
