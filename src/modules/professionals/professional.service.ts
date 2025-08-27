021d29e2b6cf5b80aba2326bd00b8a4b1ad89d78
itsnicolearias <nicolmccann@gmail.com>
2025-08-27 16:05:23 -0300

    feat: return professionals average rating

--- src/modules/professionals/professional.service.ts
+++ src/modules/professionals/professional.service.ts
@@ -10,6 +10,7 @@ import { Op } from "sequelize";
 import { WeekDay } from "../settings/profile.interface";
 import { groupByDate } from "../../utils/date-agrupator";
 import { AppointmentType } from "../../models/appointment_type";
+import { getProfessionalRating } from "../../utils/professionals-rating";
 
 class ProfessionalService extends BaseService<User> implements IProfessionalService {
     constructor() {
@@ -106,10 +107,12 @@ public async getAll(professionalId?: string | null, includeModel?: object, page?
   }
 }
 
-async getOne(where: Record<string, unknown>, includeModel?: object, professionalId?: string) {
+async getOneProfessional(where: Record<string, unknown>, includeModel?: object, professionalId?: string) {
   try {
     const professional = await super.getOne(where, [Profile, AppointmentType])
-    return professional;
+    const rating = await getProfessionalRating(String(where.userId))
+    
+    return {professional, rating};
   } catch (error) {
     throw Boom.badRequest(error);
   }
