d60cb1c11af015de2f18fb087173f6b39d168e35
itsnicolearias <nicolmccann@gmail.com>
2025-10-21 09:30:37 -0300

    fix: add pg

--- src/libs/sequelize.ts
+++ src/libs/sequelize.ts
@@ -1,10 +1,12 @@
 import { Sequelize } from 'sequelize-typescript';
 import { config } from '../config/environments';
 import { Models } from '../models';
+import pg from "pg"
 
-// Configuration of the first database
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const options: any = {
   dialect: 'postgres',
+  dialectModule: pg,
   dialectOptions: {
     application_name: 'CalendUp',
     schema: 'public',
